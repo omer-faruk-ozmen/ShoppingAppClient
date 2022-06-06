adf096460d12c59a024d72864f3752463a53390f
Omer Faruk Ozmen <omerfarukozmen0@gmail.com>
2022-06-06 22:32:38 +0300

    Bootstrap and Query Integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ShoppingAppClient';
 }
+
+$(document).ready(() => {
+  alert('Test');
+});
